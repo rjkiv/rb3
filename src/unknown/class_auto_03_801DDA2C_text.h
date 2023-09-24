6b69981c0768f1bc6e63c9b59df409d68c90702a
rjkiv <rjkiv@users.noreply.github.com>
2023-09-24 12:41:00 -0700

    begin class implementation

---
+++ src/unknown/class_auto_03_801DDA2C_text.h
@@ -0,0 +1,28 @@
+
+
+class Unknown {
+    public:
+        int unk00, unk04, unk08, unk0c;
+        int unk10, unk14, unk18, unk1c;
+        int unk20;
+        unsigned char unk24;
+        unsigned char unk25, unk26, unk27;
+        int unk28, unk2c;
+        int unk30;
+        int unk34;
+        int unk38, unk3c, unk40, unk44, unk48, unk4c, unk50, unk54, unk58, unk5c;
+        unsigned char unk60;
+        unsigned char unk61;
+
+
+        void fn_801F6CC0(int, int);             // https://decomp.me/scratch/ijAZ1
+
+        // this fxn could have an optional default variable? not sure yet
+        int fn_801F7AC0();                      // https://decomp.me/scratch/o1mis
+
+        void fn_801F73D0(int);                  // https://decomp.me/scratch/FcplV
+        void fn_801F65DC();                     // https://decomp.me/scratch/6CbAj
+
+        void fn_801F7BA8(int,int*,int,int);     // https://decomp.me/scratch/CXXUs - INCOMPLETE!!!
+
+};
