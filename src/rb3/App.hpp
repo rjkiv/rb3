886927596d21b917a7dcd2125689e4879bf2d3b4
rjkiv <rjkiv@users.noreply.github.com>
2023-09-25 11:02:44 -0700

    one more CloseToApp method found

--- src/rb3/App.hpp
+++ src/rb3/App.hpp
@@ -10,16 +10,21 @@ public:
 // found in the fxn call App::App(int, char**)
 class CloseToApp {
     public:
-    int unk0, unk4, unk8, unkc;
+    int unk0, unk4; 
+    unsigned char unk8, unk9, unka, unkb;
+    int unkc;
     float unk10, unk14;
     int unk18, unk1c;
     int unk20, unk24;
     float unk28;
     unsigned char unk2c;
 
+    // aka fn_8030F9EC
     CloseToApp(); 					// constructor: https://decomp.me/scratch/aNp4f
+
 	void fn_8000EEB8();				// https://decomp.me/scratch/Z7HaR
 	int fn_8000DB9C();				// https://decomp.me/scratch/bWvsN
+    void fn_80539A64(unsigned char); // https://decomp.me/scratch/KBRuj
 };
 
 // https://decomp.me/scratch/oDwf6 - INCOMPLETE, PLEASE FIX!!!
